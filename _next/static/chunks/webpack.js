89164c68cb3cfa0d59c32704563a7af800475440
alex <gaer87@gmail.com>
2021-07-23 23:01:09 +0300

    Build

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f4203ed2b6befd2a0ed4";
+/******/ 	var hotCurrentHash = "74d5a1447fb3cc427bad";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
